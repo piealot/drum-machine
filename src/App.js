e7c17d46166f76045dbdd3e04f6559e6be8e94b9
piealot <piter.ome@gmail.com>
2024-01-20 14:23:50 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  let audio = document.getElementById("audio");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="drum-machine" id="drum-machine">
+      <div className="pad-bank" id="pad-bank">
+        <DrumPad />
+        <DrumPad />
+        <DrumPad />
+        <DrumPad />
+        <DrumPad />
+        <DrumPad />
+        <DrumPad />
+        <DrumPad />
+        <DrumPad />
+      </div>
+      <Display />
+      <Input />
     </div>
   );
+}
+
+function DrumPad() {
+  return (
+    <div className="drum-pad" id="drum-pad">
+      <button
+        onClick={() => {
+          document.getElementById("audio").currentTime = 0;
+          document.getElementById("audio").play();
+        }}
+        class="drum-pad-button"
+      >
+        Play
+      </button>
+      <audio id="audio">
+        <source src="RP4_KICK_1.mp3" type="audio/mpeg" />
+      </audio>
+    </div>
+  );
+}
+
+function Display() {
+  return (
+    <div class="display" id="display">
+      Last sound
+    </div>
+  );
+}
+
+function handleKeyDown(event) {
+  if (event.key === "Enter") {
+    console.log("enter");
+  }
+}
+function Input() {
+  return <input onKeyDown={handleKeyDown} />;
 }
 
 export default App;
